8abfabd7cf3ed2bff5d377c7bb02639e8d81f20c
Thor K. Høgås <thor@roht.no>
2021-10-04 01:57:14 +0200

    fix(nordnet): add columns & fix for nb_NO

--- ynab_csv_converter/formats/nordnet.py
+++ ynab_csv_converter/formats/nordnet.py
@@ -1,16 +1,22 @@
 import re
+import locale
 from collections import namedtuple
 
-NordnetLine = namedtuple('NordnetLine', ['line_id', 'bogf_date', 'trns_date', 'val_date', 'trns_type',
-                                         'stock_name', 'instr_type', 'isin', 'quantity', 'price',
-                                         'interest', 'fee', 'amount', 'currency', 'buy_price',
-                                         'result', 'total_qty', 'saldo', 'exch_rate', 'trns_text',
-                                         'shred_date', 'verification_number',
+NordnetLine = namedtuple('NordnetLine', ['line_id', 'bogf_date', 'trns_date', 'val_date', 'portfolio_id',
+                                         'trns_type', 'stock_name', 'instr_type', 'isin', 'quantity', 'price',
+                                         'interest', 'fee', 'fee_currency', 'amount', 'currency',
+                                         'buy_price', 'result', 'total_qty', 'saldo', 'exch_rate', 'trns_text',
+                                         'shred_date', 'slip_number', 'verification_number', 'brkr_fee',
+                                         'brkr_fee_currency',
                                          ])
+
+thousands = r' '
 date_pattern = r'^\d{4}-\d{2}-\d{2}$'
 optional_date_pattern = r'^(\d{4}-\d{2}-\d{2}|)$'
-amount_pattern = r'^-?\d{1,3}(\.\d{3})*,\d{2,}$'
-pos_amount_pattern = r'^\d{1,3}(\.\d{3})*,\d{2,}$'
+amount_pattern = r'\d{1,3}(' + thousands + r'\d{3})*(,\d{1,})?'
+all_amount_pattern = r'^-?' + amount_pattern + r'$'
+pos_amount_pattern = r'^' + amount_pattern + r'$'
+opt_amount_pattern = r'(' + amount_pattern + r')?$'
 column_patterns = {'line_id':     r'^\d{9,}$',
                    'bogf_date':   date_pattern,
                    'trns_date':   date_pattern,
@@ -19,23 +25,30 @@ column_patterns = {'line_id':     r'^\d{9,}$',
                    'stock_name':  r'^.*$',
                    'instr_type':  r'^.*$',
                    'isin':        r'^.*$',
-                   'quantity':    r'^(\d+|)$',
+                   'quantity':    pos_amount_pattern,
                    'price':       pos_amount_pattern,
                    'interest':    pos_amount_pattern,
                    'fee':         pos_amount_pattern,
-                   'amount':      amount_pattern,
+                   'amount':      all_amount_pattern,
                    'currency':    r'^[A-Z]{3}$',
                    'buy_price':   pos_amount_pattern,
-                   'result':      amount_pattern,
-                   'total_qty':   amount_pattern,
-                   'saldo':       amount_pattern,
+                   'result':      all_amount_pattern,
+                   'total_qty':   all_amount_pattern,
+                   'saldo':       all_amount_pattern,
                    'exch_rate':   pos_amount_pattern,
                    'trns_text':   r'^.*$',
                    'shred_date':  optional_date_pattern,
+                   'slip_number': r'^\d*$',
                    'verification_number': r'^\d{8,}$',
+                   'brkr_fee':    opt_amount_pattern,
+                   'brkr_fee_currency':  r'^.*$',
                    }
 column_patterns = {column: re.compile(regex) for column, regex in column_patterns.items()}
 txn_date_descends = True
+
+
+def to_float(amount):
+    return locale.atof(amount.replace(' ', ''))
 
 
 def getlines(path):
@@ -45,11 +58,13 @@ def getlines(path):
     from . import validate_line
     from .ynab import YnabLine
 
-    with open(path, 'r', encoding='iso-8859-1') as handle:
-        transactions = csv.reader(handle, delimiter=';', quotechar='"',
-                                  quoting=csv.QUOTE_MINIMAL)
-        locale.setlocale(locale.LC_ALL, 'da_DK.UTF-8')
-        conv = {'EUR': 7.46, 'USD': 6.68, 'DKK': 1}
+    locale.setlocale(locale.LC_ALL, '')
+    global thousands
+    thousands = locale.localeconv()['grouping']
+
+    with open(path, 'r', encoding='utf-16') as handle:
+        transactions = csv.reader(handle, delimiter='\t', quoting=csv.QUOTE_MINIMAL)
+        conv = {'EUR': 7.46, 'USD': 6.68, 'DKK': 1, 'NOK': 1}
         # Skip headers
         next(transactions)
         for raw_line in transactions:
@@ -61,19 +76,23 @@ def getlines(path):
                 validate_line(line, column_patterns)
 
                 date = datetime.datetime.strptime(line.bogf_date, '%Y-%m-%d')
-                if line.instr_type == 'Aktie':
+                exch_rate = to_float(line.exch_rate)
+                local_price = to_float(line.price) * exch_rate
+                if 'aktie' in line.instr_type.lower():
                     payee = '{action} {code}'.format(action=line.trns_type.capitalize(), code=line.stock_name)
-                    memo = '{qty} stk. til {cur} {price}'.format(qty=line.quantity, price=line.price, cur=line.currency)
+                    memo = f'{line.quantity} stk. til {line.currency} {local_price}'
+                    if exch_rate != 0:
+                        memo += f' (veksl. {line.exch_rate})'
                 else:
                     payee = line.trns_type.capitalize()
                     memo = ''
                 category = ''
-                amount = round(locale.atof(line.amount) * conv[line.currency], 2)
+                amount = round(locale.atof(line.amount.replace(' ', '')) * conv[line.currency], 2)
                 if amount > 0:
                     outflow = 0.0
-                    inflow = amount
+                    inflow = abs(amount)
                 else:
-                    outflow = -amount
+                    outflow = abs(amount)
                     inflow = 0.0
             except Exception:
                 import sys
