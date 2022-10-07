811d2af568a2f0cf5d22ac14ff19b02d16c52ee8
Thor K. Høgås <thor@roht.no>
2022-10-07 17:22:16 +0200

    refactor: csv-based bank norwegian imports only

--- ynab_csv_converter/formats/banknorwegian.py
+++ ynab_csv_converter/formats/banknorwegian.py
@@ -1,12 +1,15 @@
 import csv
-import datetime
 import locale
 import re
 from collections import namedtuple
+from decimal import Decimal
+from datetime import datetime
 
 import pandas as pd
 from ynab_csv_converter.formats import validate_line
 from ynab_csv_converter.formats.ynab import YnabLine
+
+NorwegianRow = namedtuple('NorwegianRow', ["date", "payee", "type", "currency_amount", "currency_rate", "currency", "amount", "merchant_area", "merchant_category", "book_date", "value_date"])
 
 amount_pattern = r'^-?\d{1,}(\.\d{3})*(.\d{1,2})?$'
 date_pattern = r'^\d{2}\.\d{2}\.\d{4}'
@@ -37,8 +40,8 @@ def is_stocks(input_line):
 def try_improve_date(output: dict, input: namedtuple):
     match_actual_date = re.search(fr'{visa_card_pattern}? {short_date_pattern}', input.text)
     if match_actual_date:
-        payment_date = datetime.datetime.strptime(match_actual_date['date'], short_date_format)
-        output_year: datetime.datetime.year = output['date'].year
+        payment_date = datetime.strptime(match_actual_date['date'], short_date_format)
+        output_year: datetime.year = output['date'].year
         payment_date = payment_date.replace(year=output_year)
 
         # If the month is "into" the year, then it's the previous year, otherwise choo-choo
@@ -113,33 +116,37 @@ def try_improve_with_memo(output: dict, input: namedtuple):
 
 def getlines(path: str):
     # Load the transactions
-    sheet = pd.read_excel(path, sheet_name=None)
-    dfs: pd.DataFrame = sheet['transactions']
-    dfs = dfs.convert_dtypes()
-    for i, row in dfs.iterrows():
-        try:
-            result = {'date': row['TransactionDate'].to_pydatetime(),
-                      'payee': row['Text'], 'category': "",
-                      'memo': row['Merchant Category'], 'outflow': 0.0, 'inflow': 0.0}
+    with open(path, 'r', encoding='utf-8-sig') as handle:
+        transactions = csv.reader(handle, delimiter=',', quotechar='"',
+                                  quoting=csv.QUOTE_ALL)
+        locale.setlocale(locale.LC_ALL, 'no_NO.UTF-8')
 
-            amount = row['Amount']
-            result.update({
-                'outflow': 0.0 if amount > 0 else -amount,
-                'inflow': 0.0 if amount < 0 else amount,
-            })
+        next(transactions)
 
-            if amount > 0:
-                result['memo'] = ""
+        for row in transactions:
+            row = NorwegianRow(*row)
+            try:
+                result = {'date': datetime.strptime(row.date, '%Y/%m/%d'),
+                          'payee': row.payee, 'category': "",
+                          'memo': row.merchant_category, 'outflow': 0.0, 'inflow': 0.0}
 
-            if row['Currency'] != 'NOK':
-                result['memo'] += f" {row['Currency Amount']} {row['Currency']}"
+                amount = Decimal(row.amount)
+                result.update({
+                    'outflow': 0.0 if amount > 0 else -amount,
+                    'inflow': 0.0 if amount < 0 else amount,
+                })
 
-        except Exception as e:
-            import sys
-            msg = ("There was a problem on line {line} in {path}, Python line {line_code}\n"
-                   .format(line=i, path=path, line_code=sys.exc_info()[2].tb_lineno))
-            sys.stderr.write(msg)
-            raise e
+                if amount > 0:
+                    result['memo'] = ""
 
-        print(result)
-        yield YnabLine(**result)
+                if row.currency != 'NOK':
+                    result['memo'] += f" {row.currency_amount} {row.currency}"
+
+            except Exception as e:
+                import sys
+                msg = ("There was a problem on line {line} in {path}, Python line {line_code}\n"
+                       .format(line=transactions.line_num, path=path, line_code=sys.exc_info()[2].tb_lineno))
+                sys.stderr.write(msg)
+                raise e
+
+            yield YnabLine(**result)
