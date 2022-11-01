decb67633f8362fc89cc77f7dc0a3d728045c819
Thor K. Høgås <thor@roht.no>
2022-11-01 22:48:36 +0100

    fix(banknorwegian): use nb_NO locale

--- ynab_csv_converter/formats/banknorwegian.py
+++ ynab_csv_converter/formats/banknorwegian.py
@@ -119,7 +119,7 @@ def getlines(path: str):
     with open(path, 'r', encoding='utf-8-sig') as handle:
         transactions = csv.reader(handle, delimiter=',', quotechar='"',
                                   quoting=csv.QUOTE_ALL)
-        locale.setlocale(locale.LC_ALL, 'no_NO.UTF-8')
+        locale.setlocale(locale.LC_ALL, 'nb_NO.UTF-8')
 
         next(transactions)
 
